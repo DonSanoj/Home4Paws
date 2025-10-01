29f75f811d8f276f376396b73831550672ace63a
charukaonline <nuwanthakonline@gmail.com>
2025-10-01 20:50:14 +0530

    Add Header component and implement routing for new pages

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,27 +1,29 @@
-import './App.css'
-import { Route, Routes } from 'react-router-dom'
-import AboutUs from './pages/AboutUs'
-import Home from './pages/Home'
+import "./App.css";
+import { Route, Routes } from "react-router-dom";
+import AboutUs from "./pages/AboutUs";
+import Home from "./pages/Home";
+import ContactUs from "./pages/ContactUs";
+import PrivacyPolicy from "./pages/PrivacyPolicy";
+import TermsAndConditions from "./pages/TermsAndConditions";
+import Services from "./pages/Services";
+import Skills from "./pages/Skills";
+import Header from "./components/includes/Header";
 
 function App() {
-
   return (
     <>
+      <Header />
       <Routes>
-
-        <Route
-          path="/"
-          element={<Home />}
-        />
-
-        <Route 
-          path="/about"
-          element={<AboutUs />}
-        />
-
+        <Route path="/" element={<Home />} />
+        <Route path="/about" element={<AboutUs />} />
+        <Route path="/contact" element={<ContactUs />} />
+        <Route path="/privacy" element={<PrivacyPolicy />} />
+        <Route path="/terms" element={<TermsAndConditions />} />
+        <Route path="/services" element={<Services />} />
+        <Route path="/skills" element={<Skills />} />
       </Routes>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
